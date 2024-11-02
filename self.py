d295997962b7f414a09ad4c3aa7f3a90569f7700
吖嗼 <164567335@QQ.COM>
2024-11-02 10:16:09 +0800

    Update self.py

--- self.py
+++ self.py
@@ -2,7 +2,7 @@
 import requests
 import difflib
 
-urls = ["https://ghp.ci/https://raw.githubusercontent.com/YaMo335/TvSources/refs/heads/master/code/ipv4normal.txt"]
+urls = ["https://mirror.ghproxy.com/https://raw.githubusercontent.com/YaMo335/TV/refs/heads/master/output/result.txt"]
 
 final = []
 
